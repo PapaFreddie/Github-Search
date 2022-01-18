b12e088e0fd242336fc44e1a8b5328846eadc0c9
PapaFreddie <fred.papa@student.moringaschool.com>
2022-01-18 12:36:35 +0300

    added repo component to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeLandingPageComponent } from './compononents/home-landing-page/home-landing-page.component';
+import { RepoComponent } from './components/repo/repo.component';
 import { GithubComponent } from './components/github/github.component';
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     HomeLandingPageComponent,
-    GithubComponent
+    GithubComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
